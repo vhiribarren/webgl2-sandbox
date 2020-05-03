c5b8d7d84ea4d106c93cfadd9c0fdaecc06752a0
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2020-05-03 18:41:39 +0200

    First fully working webgl test

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ function createProgram(context: WebGL2RenderingContext, vertexShader: WebGLShade
             console.debug(`ProgramInfoLog Validate: ${validateMessage}`);
         }
     }
-    return programRef
+    return programRef;
 }
 
 
@@ -68,7 +68,33 @@ if (! gl) {
     console.warn("WebGL2 not available");
 }
 
-
+// Shaders
 const vtx_shader = loadShader(gl, ShaderType.vertex, vtx_glsl);
 const frg_shader = loadShader(gl, ShaderType.fragment, frg_glsl);
 const program = createProgram(gl, vtx_shader, frg_shader);
+
+// Uniforms and attributes
+const coeffUniformRef = gl.getUniformLocation(program, "coefficient");
+const vertexAttrRef = gl.getAttribLocation(program, "vertexPosition");
+
+// Square points
+const squareBufferRef = gl.createBuffer();
+const squarePoints = new Float32Array([
+    -1.0, -1.0,
+    -1.0, 1.0,
+    1.0, -1.0,
+    1.0, 1.0
+]);
+gl.bindBuffer(gl.ARRAY_BUFFER, squareBufferRef);
+gl.bufferData(gl.ARRAY_BUFFER, squarePoints, gl.STATIC_DRAW);
+
+gl.vertexAttribPointer(vertexAttrRef, 2, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(vertexAttrRef);
+
+gl.useProgram(program);
+
+gl.uniform1f(coeffUniformRef, 0.5);
+
+gl.clearColor(0.0, 0.4, 0.5, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+gl.drawArrays(gl.TRIANGLE_STRIP, 0, squarePoints.length / 2);
--- src/vertex.glsl
+++ src/vertex.glsl
@@ -1,8 +1,9 @@
 #version 300 es
  
 
-in vec4 a_position;
+in vec2 vertexPosition;
+uniform float coefficient;
 
 void main() {
-  gl_Position = a_position;
+  gl_Position = vec4(coefficient * vertexPosition, 0.0, 1.0);
 }
