4aa7743b9f9984a8ccba46d01965ae029140a902
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2020-05-08 14:51:10 +0200

    Modify viewport size

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import {SimpleSquare} from './1_simple_square/simple_square';
 
-
 const canvas = document.createElement('canvas');
-canvas.width = 500;
+canvas.width = window.innerWidth-20;;
 canvas.height = 500;
 document.body.appendChild(canvas);
 
@@ -10,6 +9,9 @@ const gl = canvas.getContext("webgl2");
 if (!gl) {
     console.warn("WebGL2 not available");
 }
+
+// Width as variable size, while height is constant for image
+gl.viewport((canvas.width - canvas.height)/2, 0, canvas.height, canvas.height);
 
 const simpleSquare = new SimpleSquare();
 simpleSquare.render(gl);
